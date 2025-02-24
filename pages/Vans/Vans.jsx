c7c375d96edf1e316374c954ed31bc414a3353e4
Collins Kimotho <collinskimotho16@gmail.com>
2025-02-24 11:51:48 +0300

    conditionally render button

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,15 +1,26 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useSearchParams } from "react-router-dom";
+
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const typeFilter = searchParams.get("type")
+    console.log(typeFilter)
+
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
 
-    const vanElements = vans.map(van => (
+    const displayedVans = typeFilter
+        ? vans.filter(van => van.type === typeFilter)
+        : vans
+
+    const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
                 <img src={van.imageUrl} />
@@ -25,6 +36,16 @@ export default function Vans() {
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-class-filter-buttons">
+                <button onClick={()=> setSearchParams("?type=rugged")} className="">Rugged</button>
+                <button onClick={()=> setSearchParams("?type=luxury")} className="">Luxury</button>
+                <button onClick={()=> setSearchParams("type=simple")} className="">Simple</button>
+                {
+                    typeFilter ? (
+                        <button onClick={()=> setSearchParams(".")} className="">Clear Filers</button>
+                    ) : null
+                   }
+            </div>
             <div className="van-list">
                 {vanElements}
             </div>
